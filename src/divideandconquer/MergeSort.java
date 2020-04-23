6b9e6fd443195c8a764294254b84041bfb33b91f
Bishwa <bishwa.nayak@sap.com>
2020-04-23 23:50:29 +0530

    correct implementation of merge sort the temp array should be created at the time of merge.

--- src/divideandconquer/MergeSort.java
+++ src/divideandconquer/MergeSort.java
@@ -5,37 +5,55 @@ package divideandconquer;
  */
 public class MergeSort {
 
-    public static void merge(int theArray[] ,int []workSpace, int lowPtr,
-                             int highPtr, int upperBound) {
-        int j = 0; // workspace index
-        int lowerBound = lowPtr;
-        int mid = highPtr-1;
-        int n = upperBound-lowerBound+1; // # of items
-        while(lowPtr <= mid && highPtr <= upperBound) {
-            if (theArray[lowPtr] < theArray[highPtr])
-                workSpace[j++] = theArray[lowPtr++];
-            else
-                workSpace[j++] = theArray[highPtr++];
+    public static void merge(int arr[] , int start,
+                             int mid, int end) {
+        // create a temp array
+        int temp[] = new int[end - start + 1];
+
+        // crawlers for both intervals and for temp
+        int i = start, j = mid+1, k = 0;
+
+        // traverse both arrays and in each iteration add smaller of both elements in temp
+        while(i <= mid && j <= end) {
+            if(arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                k += 1; i += 1;
+            }
+            else {
+                temp[k] = arr[j];
+                k += 1; j += 1;
+            }
         }
 
-        while(lowPtr <= mid)
-            workSpace[j++] = theArray[lowPtr++];
-        while(highPtr <= upperBound)
-            workSpace[j++] = theArray[highPtr++];
+        // add elements left in the first interval
+        while(i <= mid) {
+            temp[k] = arr[i];
+            k += 1; i += 1;
+        }
 
-        for(j=0; j<n; j++)
-            theArray[lowerBound+j] = workSpace[j];
+        // add elements left in the second interval
+        while(j <= end) {
+            temp[k] = arr[j];
+            k += 1; j += 1;
+        }
 
+        // copy temp to original interval
+        for(i = start; i <= end; i += 1) {
+            arr[i] = temp[i - start];
+        }
     }
 
-    public static void recMergeSort(int arr[], int nums[], int l, int h){
+    // arr is an array of integer type
+    // start and end are the starting and ending index of current interval of arr
+
+    public static void recMergeSort(int arr[], int l, int h){
 
 
         if(l<h){
             int mid = (l+h)/2;
-            recMergeSort(arr,nums, l, mid);
-            recMergeSort(arr,nums, mid+1, h);
-            merge(arr,nums,l,mid+1,h);
+            recMergeSort(arr, l, mid);
+            recMergeSort(arr, mid+1, h);
+            merge(arr,l,mid,h);
 
         }
     }
@@ -43,8 +61,7 @@ public class MergeSort {
     public static void mergeSort(int arr[]){
         int l = 0;
         int r = arr.length-1;
-        int nums[] = new int[arr.length];
-        recMergeSort(arr, nums, l, r);
+        recMergeSort(arr, l, r);
 
     }
 
