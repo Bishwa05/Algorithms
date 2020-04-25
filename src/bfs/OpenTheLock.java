f3f196fcccc10ae375f54a23d71057c9c0116410
Bishwa <bishwa.nayak@sap.com>
2020-04-26 01:16:02 +0530

    bfs

---
+++ src/bfs/OpenTheLock.java
@@ -0,0 +1,69 @@
+package bfs;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class OpenTheLock {
+
+    public int openLock(String []deadends, String target) {
+        HashSet<String> deadEnds = new HashSet<>(Arrays.asList(deadends));
+
+        HashSet<String> visited = new HashSet<>();
+        visited.add("0000");
+
+        Queue<String> q = new LinkedList();
+        q.offer("0000");
+
+        int level =0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size > 0) {
+                String lockPosition = q.poll();
+                if(deadEnds.contains(lockPosition)){
+                    size--;
+                    continue;
+                }
+
+                if(lockPosition.equals(target)){
+                    return level;
+                }
+
+                StringBuilder sb = new StringBuilder(lockPosition);
+
+                for(int i=0; i<4; i++){
+                    char currPos = sb.charAt(i);
+                    String s1 = sb.substring(0, i) + (currPos=='9'?0: currPos -'0'+1)+
+                            sb.substring(i+1);
+
+                    String s2 = sb.substring(0, i) + (currPos=='0'?9: currPos -'0'-1)+
+                            sb.substring(i+1);
+
+                    if(!visited.contains(s1) && !deadEnds.contains(s1)){
+                        q.offer(s1);
+                        visited.add(s1);
+                    }
+
+                    if(!visited.contains(s2) && !deadEnds.contains(s2)){
+                        q.offer(s2);
+                        visited.add(s2);
+                    }
+                }
+                size--;
+            }
+            level++;
+
+        }
+
+    return -1;
+
+    }
+
+    public static void main(String arg[]){
+        String arr[] ={"0201","0101","0102","1212","2002"};
+        String target ="0202";
+        OpenTheLock  o = new OpenTheLock();
+        System.out.println(o.openLock(arr, target));
+    }
+}
