85782128ac0ca6f8c62f673451185eecb6b4c12a
Bishwa <bishwa.nayak@sap.com>
2020-05-07 00:54:12 +0530

    array

---
+++ src/array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,38 @@
+package array;
+
+/**
+ * 121. Best Time to Buy and Sell Stock
+ * Leetcodee
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+//         int maxProfit =0;
+//         for(int i = prices.length-1; i>0;  i--){
+//             int profit = 0;
+//             for(int j= i-1; j>=0; j--){
+//                 if(prices[i]<prices[j]){
+//                     continue;
+//                 }
+//                 profit = Math.max(profit,prices[i]-prices[j]);
+
+//             }
+//             if(profit>maxProfit){
+//                 maxProfit = profit;
+//             }
+//             profit =0;
+//         }
+//     return maxProfit;
+
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+}
---
+++ src/dynamic/UniqueBinarySearchTrees.java
@@ -0,0 +1,20 @@
+package dynamic;
+
+/**
+ *  I think this is implementation of catalan number.
+ *  TODO: More analysis needed.
+ */
+public class UniqueBinarySearchTrees {
+
+    public int numTrees(int n) {
+
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+        for(int l = 2; l <= n; l++)
+        {
+            for(int i = 0; i < l; i++)
+                dp[l] += (dp[i] * dp[l - i - 1]);
+        }
+        return dp[n];
+    }
+}
