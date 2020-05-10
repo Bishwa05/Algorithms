23f6b312385a12a2da02bbe32d4d0447cc4e35f6
Bishwa <bishwa.nayak@sap.com>
2020-05-10 20:05:40 +0530

    Topological Sort

---
+++ src/graph/TopologicalSort.java
@@ -0,0 +1,69 @@
+package graph;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *                  A           B
+ *                    \     /   |
+ *                      C       D
+ *                     /       /
+ *                     E      /
+ *                  /   \    /
+ *                H        F
+ *                          \
+ *                          G
+ *
+ *  1. Pick any vertex u, put it in visited set.
+ *  2. if u->v, visit to v
+ *      3. Then goto step 1 for vertex v until no more path found.
+ *  4. else put u in the stack.
+ *  5. Repeat the step 1 for all the unvisited nodes.
+ *
+ */
+public class TopologicalSort<T> {
+
+
+    public Deque<Vertex<T>> topSort(Graph<T> graph) {
+        Deque<Vertex<T>> stack = new ArrayDeque<>();
+        Set<Vertex<T>> visited = new HashSet<>();
+        for (Vertex<T> vertex1 : graph.getAllVertex()) {
+            if (visited.contains(vertex1)) {
+                continue;
+            }
+            topSortUtil(vertex1,stack,visited);
+        }
+        return stack;
+    }
+
+    private void topSortUtil(Vertex<T> vertex, Deque<Vertex<T>> stack,
+                             Set<Vertex<T>> visited) {
+        visited.add(vertex);
+        for(Vertex<T> childVertex1 : vertex.getAdjacentVertexes()){
+            if(visited.contains(childVertex1)){
+                continue;
+            }
+            topSortUtil(childVertex1,stack,visited);
+        }
+        stack.offerFirst(vertex);
+    }
+
+    public static void main(String args[]){
+        Graph<Integer> graph = new Graph<>(true);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(3, 4);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 3);
+        graph.addEdge(3, 8);
+        graph.addEdge(8, 11);
+
+        TopologicalSort<Integer> sort = new TopologicalSort<>();
+        Deque<Vertex<Integer>> result = sort.topSort(graph);
+        while(!result.isEmpty()){
+            System.out.println(result.poll());
+        }
+    }
+}
