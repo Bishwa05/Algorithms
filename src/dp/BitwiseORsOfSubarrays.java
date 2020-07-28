b17a2e218e3bf839490fff071b7531978d91b479
Bishwa <bishwa.nayak@sap.com>
2020-07-28 21:31:46 +0530

    dp

--- src/dp/BitwiseORsOfSubarrays.java
+++ src/dp/BitwiseORsOfSubarrays.java
@@ -7,6 +7,23 @@ import java.util.Set;
  * Leetcode
  * 898. Bitwise ORs of Subarrays
  *
+ *
+ *
+ * The solution is straight forward,
+ * while you may worry about the time complexity up to O(N^2)
+ * However, it's not the fact.
+ * This solution has only O(30N)
+ * The reason is that, B[0][i] >= B[1][i] >= ... >= B[i][i].
+ * B[0][i] covers all bits of B[1][i]
+ * B[1][i] covers all bits of B[2][i]
+ * ....
+ * There are at most 30 bits for a positive number 0 <= A[i] <= 10^9.
+ * So there are at most 30 different values for B[0][i], B[1][i], B[2][i], ..., B[i][i].
+ * Finally cur.size() <= 30 and res.size() <= 30 * A.length()
+ * In a worst case, A = {1,2,4,8,16,..., 2 ^ 29}
+ * And all B[i][j] are different and res.size() == 30 * A.length()
+ *
+ *
  */
 
 public class BitwiseORsOfSubarrays
