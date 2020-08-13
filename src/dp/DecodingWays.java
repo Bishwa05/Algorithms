4da77d8440ffd2846886845c37a7559e6b067e63
Bishwa <bishwa.nayak@sap.com>
2020-08-13 23:31:43 +0530

    dp

---
+++ src/dp/DecodingWays.java
@@ -0,0 +1,35 @@
+package dp;
+
+/**
+ * Leetcode 91.
+ */
+public class DecodingWays
+{
+
+    public int numDecodings(String s){
+        int[] dp = new int[s.length() +1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0)=='0'?0:1;
+
+        for(int i = 2; i<=s.length(); i++){
+            int oneDigit = Integer.valueOf(s.substring(i-1, i));
+            int twoDigits = Integer.valueOf(s.substring(i-2, i));
+
+            if(oneDigit >=1){
+                dp[i] += dp[i-1];
+            }
+
+            if(twoDigits >=10 && twoDigits <=26){
+                dp[i] +=dp[i-2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String arg[]){
+        DecodingWays d = new DecodingWays();
+        System.out.println(d.numDecodings("226"));
+    }
+
+}
