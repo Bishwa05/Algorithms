74cc54d3d13b65da6b5055df95b4202f2a7f9bb9
Bishwa <bishwa.nayak@sap.com>
2020-09-03 11:38:23 +0530

    arr

---
+++ src/array/MonotonicArray.java
@@ -0,0 +1,30 @@
+package array;
+
+/**
+ * Whether array has continously increasing or decreasing elements
+ */
+public class MonotonicArray
+{
+
+    public boolean isMonotonicArray(int[] arr) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]>arr[i-1]){
+                isDecreasing = false;
+            } else{
+                isIncreasing = false;
+            }
+        }
+        return isDecreasing || isIncreasing;
+    }
+
+    public static void main(String arg[]){
+        //int[] arr = {-1, -3, -5, -7, -9};
+        //int[] arr = {1, 3, 5, 7, 9};
+        int[] arr = {1, 3, 5, 7, 6};
+        MonotonicArray m = new MonotonicArray();
+        System.out.println(m.isMonotonicArray(arr));
+    }
+}
