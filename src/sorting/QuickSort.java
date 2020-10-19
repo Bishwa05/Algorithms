aaaa7c977f58d72d58088bc35e88b26d23f164cd
Bishwa <bishwa.nayak@sap.com>
2020-10-19 10:52:40 +0530

    sort

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,5 +1,25 @@
 package sorting;
 
+/**
+ *
+ * quickSort(array, leftmostIndex, rightmostIndex)
+ *   if (leftmostIndex < rightmostIndex)
+ *     pivotIndex <- partition(array,leftmostIndex, rightmostIndex)
+ *     quickSort(array, leftmostIndex, pivotIndex)
+ *     quickSort(array, pivotIndex + 1, rightmostIndex)
+ *
+ * partition(array, leftmostIndex, rightmostIndex)
+ *   set rightmostIndex as pivotIndex
+ *   storeIndex <- leftmostIndex - 1
+ *   for i <- leftmostIndex + 1 to rightmostIndex
+ *   if element[i] < pivotElement
+ *     swap element[i] and element[storeIndex]
+ *     storeIndex++
+ *   swap pivotElement and element[storeIndex+1]
+ * return storeIndex + 1
+ *
+ *
+ */
 public class QuickSort {
 
     /** This function takes last element as pivot,
