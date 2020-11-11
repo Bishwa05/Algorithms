52f9d3193f54b033700cd20f3d63929b1a571128
Bishwa <bishwa.nayak@sap.com>
2020-11-12 01:54:19 +0530

    dp

---
+++ src/array/RotateArray.java
@@ -0,0 +1,47 @@
+package array;
+
+public class RotateArray
+{
+
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
+    public static void main(String arg[]) {
+        int arr[] ={ 1,2,3,4,5,6,7};
+        RotateArray r = new RotateArray();
+        r.rotate(arr, 3);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
