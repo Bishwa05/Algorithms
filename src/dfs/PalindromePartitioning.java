72a9f8184cafcc26cb1441ff71113a60ed13ddad
Bishwa <bishwa.nayak@sap.com>
2021-02-08 23:10:04 +0530

    dp

--- src/dfs/PalindromePartitioning.java
+++ src/dfs/PalindromePartitioning.java
@@ -51,4 +51,53 @@ public class PalindromePartitioning {
         }
         return true;
     }
+
+    /***
+     * DP approach
+     */
+
+    public static List<String> palindromePartitioning(String s){
+        List<String> result = new ArrayList<>();
+
+        if(s== null){
+            return result;
+        }
+
+        if(s.length() <= 1){
+            result.add(s);
+            return result;
+        }
+
+        int length = s.length();
+        int[][]table = new int[length][length];
+
+        for(int l =1; l<= length -1; l++) {
+            for(int i =0; i<= length -l; i++){
+                int j = i+l-1;
+                if(s.charAt(i)== s.charAt(j)){
+                    if(l ==1 || l==2){
+                        table[i][j] = 1;
+                    } else {
+                        table[i][j] = table[i+1][j-1];
+                    }
+                    if (table[i][j] == 1){
+                        result.add(s.substring(i, j+1));
+                    }
+                } else {
+                    table[i][j] = 0;
+                }
+            }
+        }
+
+    return result;
+    }
+
+    public static void main(String args[]){
+        PalindromePartitioning p = new PalindromePartitioning();
+        p.palindromePartitioning("aabbb").forEach(
+            (e) ->
+                System.out.println(e)
+
+        );
+    }
 }
