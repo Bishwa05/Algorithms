9114650df9e1a913f7ce698f51ff85d6343c5245
Bishwa <bishwa.nayak@sap.com>
2021-02-12 23:21:29 +0530

    arr

--- src/greedy/WiggleSubsequence.java
+++ src/greedy/WiggleSubsequence.java
@@ -27,4 +27,20 @@ public class WiggleSubsequence
         return 1 + Math.max(calculate(nums, 0, true),
             calculate(nums, 0, false));
     }
+
+
+    public int wiggleMaxLengthGreedy(int[] nums) {
+        if (nums.length < 2)
+            return nums.length;
+        int prevdiff = nums[1] - nums[0];
+        int count = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < nums.length; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                count++;
+                prevdiff = diff;
+            }
+        }
+        return count;
+    }
 }
