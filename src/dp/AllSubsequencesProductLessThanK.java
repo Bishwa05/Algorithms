5d1cba779863c9dc0da5d5a6ce3f24afed8c9471
Bishwa <bishwa.nayak@sap.com>
2021-03-14 03:16:24 +0530

    dp

--- src/dp/AllSubsequencesProductLessThanK.java
+++ src/dp/AllSubsequencesProductLessThanK.java
@@ -1,5 +1,8 @@
 package dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Input : [1, 2, 3, 4]
  *         k = 10
@@ -14,6 +17,54 @@ package dp;
  */
 public class AllSubsequencesProductLessThanK
 {
+    public static int computeProductDFS(int arr[], int k){
+        //int []a = new int[1];
+        return dfs(arr, k, -1, 0);
+    }
+
+    public static int dfs(int []arr, int k, int prod, int i){
+        if(i >= arr.length && 0<= prod && prod < k){
+            return 1;
+        }
+        if(i >=arr.length || prod >= k){
+            return 0;
+        }
+
+        return dfs(arr, k, prod, i+1) +
+            dfs(arr, k, (prod<0)? arr[i] : prod * arr[i], i+1);
+
+    }
+
+    /**
+     *  Memoized version.
+     */
+    public static int computeProductDFSMemo(int arr[], int k){
+        Map<String, Integer> map = new HashMap<>();
+        return helper(arr, k, -1, 0, map);
+    }
+
+    public static int helper(int []arr, int k, int prod, int i, Map<String, Integer> map){
+        String keys = prod+" "+i;
+        if(i >= arr.length && 0<= prod && prod < k){
+            return 1;
+        }
+        if(i >=arr.length || prod >= k){
+            return 0;
+        }
+
+        if(map.containsKey(keys)){
+            return map.get(keys);
+        }
+
+        int res;
+
+        res = helper(arr, k, prod, i+1, map) +
+            helper(arr, k, (prod<0)? arr[i] : prod * arr[i], i+1, map);
+        map.put(keys, res);
+        return res;
+
+    }
+
     static int computeProduct(int arr[], int k) {
 
         int n = arr.length;
@@ -41,7 +92,7 @@ public class AllSubsequencesProductLessThanK
     public static void main(String arg[]) {
         int arr1[] = {1,2,3,4};
         int k =10;
-        System.out.println(computeProduct(arr1, k));
+        System.out.println(computeProductDFS(arr1, k));
 
         int arr2[] ={4, 8, 7, 2};
         int k2 = 50;
