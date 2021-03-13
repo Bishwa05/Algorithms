e30e9ac153cbc2366eb3e0312b79e18c34f99ef3
Bishwa <bishwa.nayak@sap.com>
2021-03-13 15:25:52 +0530

    arr

--- src/dp/PredictTheWinner.java
+++ src/dp/PredictTheWinner.java
@@ -19,10 +19,31 @@ public class PredictTheWinner
         }
     }
 
+    /**
+     *
+     * DP Method
+     */
+    public boolean predictTheWinnerDP(int[] nums){
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            dp[i][i] = nums[i];
+        }
+
+        for(int len= 1; len< n; len++){
+            for(int i =0; i<n-len; i++){
+                int j = i+len;
+                dp[i][j] = Math.max(nums[i]- dp[i+1][j],
+                    nums[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] >=0;
+    }
+
     public static void main(String arg[]){
         int[] nums = {1, 5, 233, 7};
         PredictTheWinner p = new PredictTheWinner();
-        System.out.println(p.predictTheWinner(nums));
+        System.out.println(p.predictTheWinnerDP(nums));
     }
 
 }
