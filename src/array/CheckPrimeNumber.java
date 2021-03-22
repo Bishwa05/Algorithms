7844b0ffc0e017c94bda988b786a2fe3f777a089
Bishwa <bishwa.nayak@sap.com>
2021-03-22 22:23:12 +0530

    arr

---
+++ src/array/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package array;
+
+/**
+ * Time complexity O(sqrt(n)).
+ */
+public class CheckPrimeNumber
+{
+    public boolean isPrime(int number){
+        if(number < 2) return false;
+        if(number == 2) return true;
+        if(number %2 ==0) return true;
+
+        for(int i =3; i*i <= number; i+=2) {
+            if(number %i ==0) return false;
+        }
+        return true;
+    }
+}
