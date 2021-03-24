086152d99d26fd83f2bf8e4e01e1f1f5da6afc2a
Bishwa <bishwa.nayak@sap.com>
2021-03-25 00:16:08 +0530

    arr

--- src/bfs/SurroundedRegions.java
+++ src/bfs/SurroundedRegions.java
@@ -15,7 +15,7 @@ import java.util.Queue;
  */
 public class SurroundedRegions
 {
-    public void surroundedRegions(char[][] board){
+    public void solve(char[][] board){
         if(board == null || board.length ==0 || board[0].length ==0)
             return;
 
@@ -27,7 +27,7 @@ public class SurroundedRegions
 
         for(int i =0; i<m; i++){
             for(int j =0; j< n; j++){
-                if(board[i][j]=='0' && !visited[i][j]){
+                if(board[i][j]=='O' && !visited[i][j]){
                     boolean surround = true;
                     List<Integer> pointsToChange = new ArrayList<>();
                     q.add(i*n + j);// add root
@@ -45,10 +45,86 @@ public class SurroundedRegions
                             int nextX = x+dir[k][0];
                             int nextY = y +dir[k][1];
 
+                            if(nextX >=0 && nextX <m && nextY >=0 && nextY<n){
+                                if(board[nextX][nextY]=='O' && !visited[nextX][nextY]){
+                                    q.add(nextX*n+nextY);
+                                }
+                                visited[nextX][nextY] = true;
+                            } else{
+                                surround = false;
+                            }
+
                         }
 
                     }
+
+                    if(surround){
+                        for(int p : pointsToChange)
+                            board[p/n][p%n] ='X';
+                    }
                 }
+            }
+        }
+    }
+
+    /**
+     * Another approach, dfs
+     */
+    public void solveEfficientOne(char[][] board) {
+        if(board == null || board.length==0)
+            return;
+
+        int m = board.length;
+        int n = board[0].length;
+
+        //merge O's on left & right boarder
+        for(int i=0;i<m;i++){
+            if(board[i][0] == 'O'){
+                merge(board, i, 0);
+            }
+
+            if(board[i][n-1] == 'O'){
+                merge(board, i, n-1);
+            }
+        }
+
+        //merge O's on top & bottom boarder
+        for(int j=0; j<n; j++){
+            if(board[0][j] == 'O'){
+                merge(board, 0, j);
+            }
+
+            if(board[m-1][j] == 'O'){
+                merge(board, m-1, j);
+            }
+        }
+
+        //process the board
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }else if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void merge(char[][] board, int i, int j){
+        board[i][j] = '#';
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        for(int k=0; k<4; k++){
+            int x = i+dx[k];
+            int y = j+dy[k];
+
+            if(x>=0 && x<board.length
+                && y>=0 && y<board[0].length
+                && board[x][y]=='O'){
+                merge(board, x, y);
             }
         }
     }
