37cd56c774758a93fd1971db37a51497b9555ae7
Bishwa <bishwa.nayak@sap.com>
2021-04-04 14:22:16 +0530

    graph

--- src/heap/CheapestFlightWithinKStops.java
+++ src/heap/CheapestFlightWithinKStops.java
@@ -15,7 +15,9 @@ package heap;
  *
  */
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class CheapestFlightWithinKStops
 {
@@ -49,5 +51,46 @@ public class CheapestFlightWithinKStops
             }
         }
         return -1;
+    }
+
+
+    public int findCheapestPrice2(int n, int[][] flights, int src, int dst, int K) {
+        int min = Integer.MAX_VALUE;
+        int[][] graph = new int[n][n];
+        int[] minPrices = new int[n];
+        for (int[] flight : flights) {
+            int start = flight[0];
+            int end = flight[1];
+            graph[start][end] = flight[2];
+        }
+        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> prices = new LinkedList<>();
+        q.add(src);
+        prices.add(0);
+        while (!q.isEmpty() && K >= 0) {
+            K--;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int start = q.poll();
+                int priceTillNow = prices.poll();
+                for (int j = 0; j < n; j++) {
+                    int price = graph[start][j];
+                    if (price > 0) {    //there is a flight
+                        int curPrice = priceTillNow + price;
+                        int oldPrice = minPrices[j];
+                        //If there is a cheaper flight with lesser stops do not add it to q
+                        if (oldPrice == 0 || oldPrice > curPrice) {//pruning
+                            minPrices[j] = curPrice;
+                            q.add(j);
+                            prices.add(curPrice);
+                            if (j == dst && min > curPrice) {
+                                min = curPrice;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
 }
