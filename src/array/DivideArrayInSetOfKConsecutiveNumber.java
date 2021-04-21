70c56dd2655cbc20f374a7a6670c1894f0ed3ee7
Bishwa <bishwa.nayak@sap.com>
2021-04-21 22:57:33 +0530

    arr

--- src/array/DivideArrayInSetOfKConsecutiveNumber.java
+++ src/array/DivideArrayInSetOfKConsecutiveNumber.java
@@ -4,6 +4,34 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *
+ * Leetcode
+ * 1296. Divide Array in Sets of K Consecutive Numbers
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,3,4,4,5,6], k = 4
+ * Output: true
+ * Explanation: Array can be divided into [1,2,3,4] and [3,4,5,6].
+ * Example 2:
+ *
+ * Input: nums = [3,2,1,2,3,4,3,4,5,9,10,11], k = 3
+ * Output: true
+ * Explanation: Array can be divided into [1,2,3] , [2,3,4] , [3,4,5] and [9,10,11].
+ * Example 3:
+ *
+ * Input: nums = [3,3,2,2,1,1], k = 3
+ * Output: true
+ * Example 4:
+ *
+ * Input: nums = [1,2,3,4], k = 3
+ * Output: false
+ * Explanation: Each array should be divided in subarrays of size 3.
+ *
+ *
+ */
 public class DivideArrayInSetOfKConsecutiveNumber
 {
 
@@ -47,5 +75,12 @@ public class DivideArrayInSetOfKConsecutiveNumber
             }
         }
         return true;
+    }
+
+    public static void main(String arg[]){
+        int[] nums = {1,2,3,3,4,4,5,6};
+
+        DivideArrayInSetOfKConsecutiveNumber d = new DivideArrayInSetOfKConsecutiveNumber();
+        System.out.println(d.isPossibleDivide(nums, 4));
     }
 }
---
+++ src/array/MinimumOperationsToMakeTheArrayIncreasing.java
@@ -0,0 +1,30 @@
+package array;
+
+public class MinimumOperationsToMakeTheArrayIncreasing
+{
+
+    public int minOperations(int[] nums) {
+        if(nums.length ==1) return 0;
+
+        int prev = nums[0];
+        int count = 0;
+
+        for(int i = 1; i< nums.length; i++){
+            if(prev>=nums[i]){
+                int diff = prev-nums[i] +1;
+                count += diff;
+                prev = nums[i] + diff;
+            } else {
+                prev = nums[i];
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String arg[]){
+        MinimumOperationsToMakeTheArrayIncreasing m = new MinimumOperationsToMakeTheArrayIncreasing();
+        int nums[] = {1,5,2,4,1};
+        System.out.println(m.minOperations(nums));
+    }
+}
