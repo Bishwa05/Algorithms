80069dec6634e12c4ae72b3b9967c75b5f337cd6
Bishwa <bishwa.nayak@sap.com>
2021-05-18 23:45:21 +0530

    arr

--- src/array/CheckPrimeNumber.java
+++ src/array/CheckPrimeNumber.java
@@ -8,7 +8,7 @@ public class CheckPrimeNumber
     public boolean isPrime(int number){
         if(number < 2) return false;
         if(number == 2) return true;
-        if(number %2 ==0) return true;
+        if(number %2 ==0) return false;
 
         for(int i =3; i*i <= number; i+=2) {
             if(number %i ==0) return false;
