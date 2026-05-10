3be3906d3ff60b0dbd4c6101da0a02408f65de11
bishwa <naya.bishwa@gmail.com>
2026-05-10 18:16:41 +0530

    optimal approach of LRU

--- src/list/LRUCache.java
+++ src/list/LRUCache.java
@@ -1,66 +1,90 @@
 package list;
 
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class LRUCache {
+    private static class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
 
-    static Deque<Integer> deque;
-    static HashSet<Integer> hashSet;
-
-    static int size;
-
-    LRUCache(int n){
-        deque = new LinkedList<>();
-        hashSet = new HashSet<>();
-        this.size = n;
+        Node(int k, int v) {
+            key = k;
+            val = v;
+        }
     }
 
+    private final int capacity;
+    private final HashMap<Integer, Node> map;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int n){
+        this.capacity = n;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertAtHead(node);
+        return node.val;
+    }
+
+    public void put(int key, int val) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.val = val;
+            remove(node);
+            insertAtHead(node);
+        } else {
+            if (map.size() == capacity) {
+                map.remove(tail.prev.key);
+                remove(tail.prev);
+            }
+            Node newNode = new Node(key, val);
+            map.put(key, newNode);
+            insertAtHead(newNode);
+        }
+
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertAtHead(Node node) {
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
 
     // By default the less accessed element will be present at the end in the queue
-    public void put(int x) {
-        if(!hashSet.contains(x)){
-            if(deque.size() == this.size){
-                int last = deque.removeLast();
-                hashSet.remove(last);
-            }
-        } else {
-            //element added is already present, that needs to be moved to front
 
-            Iterator<Integer> itr = deque.iterator();
-            while(itr.hasNext()) {
-                if(x == itr.next()){
-                    break;
-                }
-            }
-            deque.remove(x);
-
-        }
-        deque.push(x);
-        hashSet.add(x);
-    }
 
     public void display() {
-        Iterator<Integer> it = deque.iterator();
-        while(it.hasNext()){
-            System.out.print(it.next()+" ");
-        }
-        System.out.println("");
+
     }
 
     public static void main(String arg[]) {
         LRUCache c = new LRUCache(4);
 
-        c.put(2);
-        c.put(3);
-        c.put(4);
-        c.put(5);
-        c.display();
-        c.put(6);
-        c.put(4);
-        c.display();
+//        c.put(2);
+//        c.put(3);
+//        c.put(4);
+//        c.put(5);
+//        c.display();
+//        c.put(6);
+//        c.put(4);
+//        c.display();
 
 
     }
