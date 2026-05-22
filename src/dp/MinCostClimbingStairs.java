5be4e04cd5a5dab530d36fc6ab73f609720701a8
Bishwa05 <nayak.bishwa@gmail.com>
2026-05-22 16:22:15 +0530

    leetcode problem

--- src/dp/MinCostClimbingStairs.java
+++ src/dp/MinCostClimbingStairs.java
@@ -28,6 +28,24 @@ public class MinCostClimbingStairs
         return dp[n];
     }
 
+    // Approach 2
+    public static int minCostClimbingStairs2 (int[] cost){
+        if (cost.length == 1) return cost[0];
+        if (cost.length == 2) return Math.min(cost[0], cost[1]);
+
+        int first = cost[0];
+        int second = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            int current = Math.min(cost[i], cost[i-1]) + cost[i];
+            first = second;
+            second = current;
+        }
+
+        // Minimum cost to reach last step or one step past it
+        return Math.min(first, second);
+    }
+
     public static void main (String arg[])
     {
         int[] arr = { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
